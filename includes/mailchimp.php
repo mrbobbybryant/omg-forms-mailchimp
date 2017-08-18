210f21595cffe2d22c24a8df40481a5f6be9b0ec
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-08-18 17:19:37 -0400

    update addon to use no code filter hook in rest endpoint

--- includes/mailchimp.php
+++ includes/mailchimp.php
@@ -4,7 +4,7 @@ namespace OMGForms\Mailchimp\API;
 use \DrewM\MailChimp\MailChimp;
 use OMGForms\Mailchimp\Helpers;
 
-function save_form_as_mailchimp( $args, $form ) {
+function save_form_as_mailchimp( $result, $args, $form ) {
 
 	if ( $form[ 'form_type' ] === 'mailchimp' ) {
 		$MailChimp = new MailChimp( get_option( 'mailchimp_api_key' ) );
@@ -18,16 +18,16 @@ function save_form_as_mailchimp( $args, $form ) {
 
 		$result = $MailChimp->post( "lists/" . $form['list_id'] . "/members", $data );
 
-		if ( isset( $result[ 'status' ] ) && 'subscribed' === $result[ 'status' ] ) {
-			return true;
-		} else {
-			return new \WP_Error(
+		if ( ! isset( $result[ 'status' ] ) || 'subscribed' !== $result[ 'status' ] ) {
+			$result = new \WP_Error(
 				'omg_form_mailchimp_fail',
 				'Mailchimp submission Failed.',
 				array( 'status' => 400, 'erorr' => $result )
 			);
 		}
 	}
+
+	return $result;
 }
 
-add_action( 'omg_forms_save_data', __NAMESPACE__ .  '\save_form_as_mailchimp', 10, 2 );
+add_action( 'omg_forms_save_data', __NAMESPACE__ .  '\save_form_as_mailchimp', 10, 3 );
